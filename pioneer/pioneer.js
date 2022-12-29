793d38c98227a430d332ba9c7b1ec91bf2957ce5
mhdarshad2000 <mhdarshad2937@gmail.com>
2022-12-29 20:01:10 +0530

    pioneer completed

--- pioneer/pioneer.js
+++ pioneer/pioneer.js
@@ -49,88 +49,27 @@ async function detailsPage(cityUrl, brand) {
             const $ = cheerio.load(htmlString)
             const postDiv = $(".post")
 
-            const list = $(postDiv).find("ul")
-            if ($(list).text()) {
-                $(list).each((i, serviceCenter) => {
-                    arr[i] = {}
-                    let address = []
-                    let phone = []
-                    $(serviceCenter).children("li").each((j, service) => {
-                        if (j === 0) {
-                            arr[i]["serviceCenter"] = $(service).text().replaceAll("\n", "").replaceAll("\t").trim()
-                        } else if (/[a-z]/gi.test($(service).text())) {
-                            address.push($(service).text())
-                        } else {
-                            phone.push($(service).text())
-                        }
-                    })
-                    arr[i]["address"] = address.join("  ")
-                    arr[i]["phone"] = phone.join()
+            const tableDiv = $(postDiv).find("table > tbody > tr")
+
+            if($(tableDiv).text()){
+                $(tableDiv).each((i,serviceCenter)=>{
+                    arr [i] = {}
+                    const serviceCenterName = $(serviceCenter).children("td").first().children("strong").text()
+                    arr[i]["serviceCenter"] = serviceCenterName 
+                    arr[i]["address"] = $(serviceCenter).children("td").first().text().replace(serviceCenterName,"").split("(")[0].replaceAll("\n","").replaceAll("         "," ").replace("-","").replaceAll("\t","").trim()
+                    arr[i]["phone"] = $(serviceCenter).children("td").first().text()?.split("(")[1] ? "("+ $(serviceCenter).children("td").first().text()?.split("(")[1]?.split("\n")[0]?.trim() :""
                 })
-            } else {
-                let count = 0
-                $(postDiv).children("div:not(.advlaterale)").each((i, serviceCenter) => {
-                    if ($(serviceCenter).text().length) {
-                        if ($(serviceCenter).children("div").length) {
-                            $(serviceCenter).children("div").each((j, service) => {
-                                arr[j] = {}
-                                const string = $(service).html().split("<br>")
-                                let address = []
-                                let phone = []
-                                arr[j]["serviceCenter"] = string
-                                string.map((elem,index)=>{
-                                    if(index === 1){
-                                        arr[j]["serviceCenter"] = elem.replaceAll("amp;","")
-                                    }else if(/[a-z]/gi.test(elem)){
-                                        address.push(elem)
-                                    }else{
-                                        phone.push(elem)
-                                    }
-                                })
-                                arr[j]["address"] = address.join("  ").trim()
-                                arr[j]["phone"] = phone.join().replaceAll(",","").replaceAll("\n","").replaceAll("\t","").trim()
-                            })
-                            } else {
-                                arr[count] = {}
-                                
-                                // The using of .text() is a worst case so, took .html() and edited it as needed.
-                                // According to the certain scenarios the if and else used to get the data correctly
-
-                                const string = $(serviceCenter).html().split("<br>")
-                                let address = []
-                                let phone = []
-                                string.map((elem, index) => {
-                                    if (elem.includes("<li>")) {
-                                        const lsitText = elem.split("\n")
-                                        lsitText.map((element, j) => {
-                                            if (j === 0) {
-                                                arr[count]["serviceCenter"] = element.replace("amp;", "")
-                                            } else {
-                                                const temp = element.replace("<li>", "").replace("</li>", "")
-                                                if (/[a-z]/gi.test(temp)) {
-                                                    address.push(temp.replaceAll("  ", ""))
-                                                } else {
-                                                    phone.push(temp)
-                                                }
-                                            }
-                                        })
-                                    } else if (index === 1) {
-                                        arr[count]["serviceCenter"] = elem
-                                    } else if (index > 1 && /[a-z]/gi.test(elem)) {
-                                        address.push(elem)
-                                    } else {
-                                        phone.push(elem)
-                                    }
-                                })
-
-                                arr[count]["address"] = address.join("  ")
-                                arr[count]["phone"] = phone.join().replaceAll(",", "").replaceAll("\n", "").replaceAll("\t", "").trim()
-                                count++
-                        }
+            }else{
+                $(postDiv).children("p.elenchi").each((i,serviceCenter)=>{
+                    if(!$(serviceCenter).find("span > strong").text().includes("Support for Pioneer products")){
+                        arr[i] ={}
+                        arr[i]["serviceCenter"] = $(serviceCenter).find("span > strong").text()
+                        arr[i]["address"] = $(serviceCenter).find("span > span").text()
+                        arr[i]["phone"] = $(serviceCenter).find("span[itemprop='telephone']").text()
                     }
                 })
             }
-
+            
             resolve(arr)
         } catch (error) {
             console.log(error.message)
